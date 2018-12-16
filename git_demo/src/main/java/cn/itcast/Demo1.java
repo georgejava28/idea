0c18d8f9f31d50755ee3432afea6202260db53fd
george <george_lixianzhi@163.com>
2018-12-16 20:40:47 +0800

    修改i

--- git_demo/src/main/java/cn/itcast/Demo1.java
+++ git_demo/src/main/java/cn/itcast/Demo1.java
@@ -3,5 +3,7 @@ package cn.itcast;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        int i =10;
+        System.out.println(i+1);
     }
 }
